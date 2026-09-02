b1a2fb5d47c04de180fc437286ca317b405e5f11
raviverma1810 <er.rvtech@gmail.com>
2026-09-02 21:22:53 +0530

    order update and user fix

--- src/schemas/TagsSchema.ts
+++ src/schemas/TagsSchema.ts
@@ -37,7 +37,7 @@ const TagsSchema = new Schema<TagsAttributes>(
     },
     background_color: {
       type: String,
-      maxlength: 7,
+      maxlength: 250,
     },
     background_image_url: {
       type: String,
